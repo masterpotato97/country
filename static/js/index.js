a282a2538140688102cd7100db3931846f9c75ee
Daniel Herrera <dannyherrea97@gmail.com>
2023-06-08 18:53:00 -0500

    coat

--- static/js/index.js
+++ static/js/index.js
@@ -9,18 +9,15 @@ document.getElementById('country-form').addEventListener('submit', function(even
       var country = data[0];
       
       document.getElementById('flag').innerHTML = '<img src="' + country.flags.png + '" alt="Country Flag">';
-      document.getElementById('coat-of-arms').innerHTML = '<img src="' + country.flags.svg + '" alt="Coat of Arms">';
+      document.getElementById('coat-of-arms').innerHTML = '<img src="' + country.coatOfArms.png + '" alt="Coat of Arms">';
       
       var currencyTypes = Object.values(country.currencies).map(currency => currency.name);
       document.getElementById('currency').textContent = 'Currency: ' + currencyTypes.join(', ');
-      
       
       document.getElementById('capital').textContent = 'Capital: ' + country.capital.join(',');
       
       var languages = Object.values(country.languages).map(language => language);
       document.getElementById('languages').textContent = 'Languages: ' + languages.join(', ');
-      
-      
 
     })
     .catch(error => {
